49c61d2b9a972a70c8432305d2a484ba05180db8
synich <shu_wang@163.com>
2019-06-26 22:00:33 +0800

    add lua adapt headfile for 5.1/5.2/5.3 difference

---
+++ include/luaadapt.h
@@ -0,0 +1,24 @@
+#define LUA_LIB
+#include <lua.h>
+#include <lualib.h>
+#include <lauxlib.h>
+
+#if LUA_VERSION_NUM > 501
+/*
+** Lua 5.2
+*/
+#define lua_strlen lua_rawlen
+/* luaL_typerror always used with arg at ndx == NULL */
+#define luaL_typerror(L,ndx,str) luaL_error(L,"bad argument %d (%s expected, got nil)",ndx,str)
+/* luaL_register used once, so below expansion is OK for this case */
+#define luaL_register(L,name,reg) lua_newtable(L);luaL_setfuncs(L,reg,0)
+/* luaL_openlib always used with name == NULL */
+#define luaL_openlib(L,name,reg,nup) luaL_setfuncs(L,reg,nup)
+
+#if LUA_VERSION_NUM > 502
+/*
+** Lua 5.3
+*/
+#define luaL_checkint(L,n)  ((int)luaL_checkinteger(L, (n)))
+#endif
+#endif
